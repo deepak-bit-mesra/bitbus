cf851ec3c3f5015fa349d2414598ec96f7fc2300
Deepak Kumar <deepak986698@gmail.com>
2019-07-25 23:29:07 +0530

    Databse connection changes for heroku

--- db.py
+++ db.py
@@ -4,11 +4,18 @@ import mysql.connector
 
 class DbConn():
     
+    # connection = mysql.connector.connect(
+    #     host='localhost',
+    #     user='root',
+    #     passwd='root',
+    #     database='bitbus'
+    # )
+
     connection = mysql.connector.connect(
-        host='localhost',
-        user='root',
-        passwd='root',
-        database='bitbus'
+        host='us-cdbr-iron-east-02.cleardb.net',
+        user='bc658df5e46833',
+        passwd='7ff652b58b56936',
+        database='heroku_219aea5cf37fe0e'
     )
     
     
